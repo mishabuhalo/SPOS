46b78b33c589d040f34ce1b30cbddf555f4a37d9
Misha Bukhalo <bukhalo1999@gmai.com>
2019-10-30 17:57:49 +0200

    Raise crating pipes and processes to individual functions

--- Lab 1/Lab 1/Lab 1.cpp
+++ Lab 1/Lab 1/Lab 1.cpp
@@ -7,35 +7,11 @@
 #include <tchar.h>
 
 using namespace std;
-int main()
+HANDLE CreateNamedPipeForFunctions(PROCESS_INFORMATION piProcInfo, STARTUPINFO SI, HANDLE hPipe, LPTSTR PipeName)
 {
-	PROCESS_INFORMATION piProcInfo;
-	STARTUPINFO SI;
-	wstring CommandLine(L"function_f.exe");
-	LPTSTR ClientName = &CommandLine[0];
-	HANDLE hPipe;
-	wstring PipeAdress(L"\\\\.\\pipe\\MyPipe");
-	LPTSTR PipeName = &PipeAdress[0];
-	char Buff[255];
-	DWORD iNumBytesToRead = 255, i;
+	
 
-
-	PROCESS_INFORMATION piProcInfoG;
-	STARTUPINFO SIG;
-	wstring CommandLineG(L"function_g.exe");
-	LPTSTR ClientNameG = &CommandLineG[0];
-	HANDLE hPipeG;
-	wstring PipeAdressForForFunctionG(L"\\\\.\\pipe\\MyPipeForG");
-	LPTSTR PipeNameG = &PipeAdressForForFunctionG[0];
-	char BuffG[255];
-	DWORD iNumBytesToReadG = 255, k;
-
-
-	ZeroMemory(&SI, sizeof(STARTUPINFO));
-	SI.cb = sizeof(STARTUPINFO);
-	ZeroMemory(&piProcInfo, sizeof(piProcInfo));
-
-	hPipe = CreateNamedPipe(	
+	hPipe = CreateNamedPipe(
 		PipeName,			   // имя канала
 		PIPE_ACCESS_DUPLEX,       // чтение и запись из канала
 		PIPE_TYPE_MESSAGE |       // передача сообщений по каналу
@@ -51,55 +27,62 @@ int main()
 	{
 		printf("CreatePipe failed: error code %d\n", (int)GetLastError());
 	}
+	return hPipe;
+}
 
+void CreateProcessForFunctions(PROCESS_INFORMATION piProcInfo, STARTUPINFO SI, LPTSTR ClientName)
+{
 	if ((CreateProcess(NULL, ClientName, NULL, NULL, FALSE, 0, NULL, NULL, &SI, &piProcInfo)) == 0)
 	{
 		printf("create client process: error code %d\n", (int)GetLastError());
 	}
+}
+int main()
+{
+	PROCESS_INFORMATION piProcInfoF;
+	STARTUPINFO SIF;
+	wstring CommandLineF(L"function_f.exe");
+	LPTSTR ClientNameF = &CommandLineF[0];
+	HANDLE hPipeF = NULL;
+	wstring PipeAdressF(L"\\\\.\\pipe\\MyPipe");
+	LPTSTR PipeNameF = &PipeAdressF[0];
 
-	if ((ConnectNamedPipe(hPipe, NULL)) == 0)
+	ZeroMemory(&SIF, sizeof(STARTUPINFO));
+	SIF.cb = sizeof(STARTUPINFO);
+	ZeroMemory(&piProcInfoF, sizeof(piProcInfoF));
+
+	/*char Buff[255];
+	DWORD iNumBytesToRead = 255, i;*/
+
+	hPipeF = CreateNamedPipeForFunctions(piProcInfoF, SIF, hPipeF, PipeNameF);
+	CreateProcessForFunctions(piProcInfoF, SIF, ClientNameF);
+
+	if ((ConnectNamedPipe(hPipeF, NULL)) == 0)
 	{
 		printf("client F could not connect\n");
 	}
 
+
+	PROCESS_INFORMATION piProcInfoG;
+	STARTUPINFO SIG;
+	wstring CommandLineG(L"function_g.exe");
+	LPTSTR ClientNameG = &CommandLineG[0];
+	HANDLE hPipeG = NULL;
+	wstring PipeAdressForForFunctionG(L"\\\\.\\pipe\\MyPipeForG");
+	LPTSTR PipeNameG = &PipeAdressForForFunctionG[0];
+
 	ZeroMemory(&SIG, sizeof(STARTUPINFO));
 	SIG.cb = sizeof(STARTUPINFO);
-	ZeroMemory(&piProcInfo, sizeof(piProcInfo));
+	ZeroMemory(&piProcInfoG, sizeof(piProcInfoG));
 
-	hPipeG = CreateNamedPipe(
-		PipeNameG,			   // имя канала
-		PIPE_ACCESS_DUPLEX,       // чтение и запись из канала
-		PIPE_TYPE_MESSAGE |       // передача сообщений по каналу
-		PIPE_READMODE_MESSAGE |   // режим чтения сообщений 
-		PIPE_WAIT,                // синхронная передача сообщений 
-		PIPE_UNLIMITED_INSTANCES, // число экземпляров канала 
-		4096,			   // размер выходного буфера
-		4096,		          // размер входного буфера  
-		NMPWAIT_USE_DEFAULT_WAIT, // тайм-аут клиента 
-		NULL);                    // защита по умолчанию
-
-	if (hPipeG == INVALID_HANDLE_VALUE)
-	{
-		printf("CreatePipe failed: error code %d\n", (int)GetLastError());
-	}
-
-	if ((CreateProcess(NULL, ClientNameG, NULL, NULL, FALSE, 0, NULL, NULL, &SIG, &piProcInfoG)) == 0)
-	{
-		printf("create client G process: error code %d\n", (int)GetLastError());
-	}
-
-	if ((ConnectNamedPipe(hPipeG, NULL)) == 0)
-	{
-		printf("client G could not connect\n");
-	}
-
-	
+	hPipeG = CreateNamedPipeForFunctions(piProcInfoG, SIG, hPipeG, PipeNameG);
+	CreateProcessForFunctions(piProcInfoG, SIG, ClientNameG);
 
 	char BuffToClient[] = "Message from server For F";
 	char BuffToClientG[] = "Message from server For G";
 	DWORD NumBytesToWriteToCliForG;
 	DWORD NumBytesToWriteToCli;
-	WriteFile(hPipe, BuffToClient, strlen(BuffToClient), &NumBytesToWriteToCli, NULL);
+	WriteFile(hPipeF, BuffToClient, strlen(BuffToClient), &NumBytesToWriteToCli, NULL);
 	_getch();
 	WriteFile(hPipeG, BuffToClientG, strlen(BuffToClientG), &NumBytesToWriteToCliForG, NULL);
 
